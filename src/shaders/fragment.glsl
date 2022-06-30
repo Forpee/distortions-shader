cc868fdaacb0155601c8b3934a35b6181d80b2e1
Forpee <khalilhassam3@gmail.com>
2022-06-30 17:23:11 +0200

    a

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -1,8 +1,16 @@
 uniform float uTime;
+uniform sampler2D image;
+uniform sampler2D displacement;
 
 varying vec2 vUv;
 
 void main()
 {
-    gl_FragColor = vec4(vUv, 1.0, 1.0);
+    vec4 displace=texture2D(displacement,vUv.yx);
+    
+    vec2 displacedUv=vec2(vUv.x,vUv.y);
+    
+    displacedUv.y=mix(vUv.y);
+    
+    gl_FragColor=vec4(vUv,1.,1.);
 }
